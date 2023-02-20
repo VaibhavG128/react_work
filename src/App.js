e3544d4c6a23b67cba8302ad6d3332ec27565990
vaibhav garg <vaibhavg1280@gmail.com>
2023-02-20 23:51:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Header from './components/Header';
+// import Todos from './components/Todos';
+// import Footer from './components/Footer';
+import Home from './pages/Home';
+function App() {
+ 
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../css/HomePageCSS.css'
+import {Search} from '@mui/material/styles'
+const Navbar = () => {
+
+    return (
+    <div className='wrapper'>
+    <div className='left'>
+      <Search></Search>
+    <input className='searchbar'></input></div>
+    <div className='center'>
+    <h1 className='logo'>SOLDO</h1>
+    </div><div className='menuitem'>REGISTER</div>  
+    <div className='menuitem'>SIGN IN</div>    
+    <div className='menuitem'>
+      <div className='base'>
+        <div className='indicator'>
+          <div className='notify_count'>3</div>
+        </div>
+      </div>
+
+    </div>
+       
+     </div>
+  )
+}
+
+export default Navbar
